9c4b92cf97ed65d9704abbf79582036bdc3592c1
arihabs <arihabs@gmail.com>
2023-11-01 10:04:10 -0400

    Implemented all PointSET public methods. Added some unit testing, but should be compared against more test files.

--- src/PointSET.java
+++ src/PointSET.java
@@ -1,4 +1,5 @@
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdIn;
@@ -23,10 +24,15 @@ public class PointSET{
     } //# points in set
 
     public void insert(Point2D p){
+        if(p==null)
+            throw new IllegalArgumentException("Null argument");
+
         set.add(p);
     } // add point to set
 
     public boolean contains(Point2D p){
+        if(p==null)
+            throw new IllegalArgumentException("Null argument");
         return set.contains(p);
     }// does set contain point p?
 
@@ -39,46 +45,64 @@ public class PointSET{
 //        StdDraw.show();
     }
 
-//    public Iterable<Point2D> range(RectHV rect){} //all points that are inside (or on boundary of) the rectangle
+    public Iterable<Point2D> range(RectHV rect){
+        if(rect == null)
+            throw new IllegalArgumentException("Null argument");
 
-//    public Point2D nearest(Point2D p){} // nearest neighbor in the set to point p; null if the set is empty
+        Queue<Point2D> q = new Queue<Point2D>();
+        for(Point2D p : set){
+            if(rect.contains(p))
+                q.enqueue(p);
+        }
+        return q;
+    } //all points that are inside (or on boundary of) the rectangle
+
+    public Point2D nearest(Point2D p){
+        if(p==null)
+            throw new IllegalArgumentException("Null argument");
+
+        if(this.isEmpty())
+            return null;
+
+        double minDist = Double.POSITIVE_INFINITY;
+        Point2D minP = null;
+        for(Point2D pSet : set){
+            double currDist = pSet.distanceSquaredTo(p);
+            if(currDist < minDist){
+                minDist = currDist;
+                minP = pSet;
+            }
+        }
+        return minP;
+    } // nearest neighbor in the set to point p; null if the set is empty
 
     public static void main(String[] args){
         PointSET pSet = new PointSET();
+        StdOut.println("pSet.isEmpty() = " + pSet.isEmpty());
+        StdOut.println("pSet.size() = " + pSet.size());
+        Point2D p = new Point2D(2.0,1.0);
+        StdOut.println("pSet.contains(Point(1.0,2.0)) = " + pSet.contains(p));
+        StdOut.println("pSet.nearest(Point(1.0,2.0))) = " + pSet.nearest(p));
         while(!StdIn.isEmpty()){
             double x = StdIn.readDouble();
             double y = StdIn.readDouble();
-            Point2D p = new Point2D(x,y);
+            p = new Point2D(x,y);
+//            Point2D p = new Point2D(x,y);
             pSet.insert(p);
         }
 
-        StdDraw.setScale(-.05, 1.05);
+        Point2D pPert = new Point2D(p.x()+1e-4,p.y()+1e-4);
+        StdOut.println("pSet.isEmpty() = " + pSet.isEmpty());
+        StdOut.println("pSet.size() = " + pSet.size());
+        StdOut.println("pSet.contains(Point(last added point)) = " + pSet.contains(p));
+        StdOut.println("pSet.nearest(last added point) = " + pSet.nearest(p));
+        StdOut.println("pSet.contains(Point(last added point + delX,delY)) = " + pSet.contains(pPert));
+        StdOut.println("pSet.nearest(last added point + delX,delY) = " + pSet.nearest(pPert));
+
+//        StdDraw.setScale(-.05, 1.05);
         StdDraw.enableDoubleBuffering();
         StdDraw.setPenRadius(0.01);
         pSet.draw();
         StdDraw.show();
-
-/*        while (true){
-            if(StdDraw.isMousePressed()){
-                double x = StdDraw.mouseX();
-                double y = StdDraw.mouseY();
-                StdOut.printf("%8.6f %8.6f\n",x,y);
-                Point2D p = new Point2D(x,y);
-                pSet.insert(p);
-                StdDraw.pause(5);
-//                StdDraw.clear();
-//                pSet.draw();
-//                StdDraw.show();
-            }
-            else
-                break;
-
-        }
-        StdDraw.setScale(-.05, 1.05);
-        StdDraw.enableDoubleBuffering();
-        StdDraw.setPenRadius(0.01);
-        pSet.draw();
-        StdDraw.show();*/
-//        StdOut.println("Hello World!");
     }
 }
